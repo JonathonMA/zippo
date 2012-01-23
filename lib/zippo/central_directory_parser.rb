e1a53f3b2ce5847844db5b5e37fa9d558b667afe
Jonathon M. Abbott <jma@dandaraga.net>
2012-01-23 23:45:46 +1000

    Implement end_of_cd_record_position

--- lib/zippo/central_directory_parser.rb
+++ lib/zippo/central_directory_parser.rb
@@ -1,7 +1,23 @@
 module Zippo
   class CentralDirectoryParser
+    SIGNATURE = 0x06054b50
+    PACKED_SIGNATURE = [SIGNATURE].pack('L')
+    MAX_COMMENT_LENGTH = 1<<16
     def initialize(io)
       @io = io
+    end
+    def parse_entries
+      @io.seek end_of_cd_record_position
+      eocdr = parse_end_of_cd_record @io.read
+    end
+    def end_of_cd_record_position
+      # XXX implement optimised scanning at -22 position
+      scan_size = [@io.size, MAX_COMMENT_LENGTH].min
+      at = @io.size - scan_size
+      @io.seek(at)
+      @io.read(scan_size).rindex(PACKED_SIGNATURE).tap do |pos|
+        raise "End of Central Directory Record not found" unless pos
+      end
     end
   end
 end
--- spec/central_directory_parser_spec.rb
+++ spec/central_directory_parser_spec.rb
@@ -4,13 +4,28 @@ require "zippo/central_directory_parser"
 
 module Zippo
   describe CentralDirectoryParser do
-    let(:file) { test_file "test.zip" }
     let(:io) { File.open(file) }
     let(:parser) { CentralDirectoryParser.new(io) }
     after(:each) { io.close }
 
-    it "should parse the Central Directory entries" do
-      parser.parse_entries.should have(1).entry
+    context "when parsing a simple file" do
+      let(:file) { test_file "test.zip" }
+      it "should parse the Central Directory entries" do
+        pending "need eocdr locator"
+        parser.parse_entries.should have(1).entry
+      end
+
+      specify { parser.end_of_cd_record_position.should eq 0xbf }
+    end
+
+    context "when parsing a file with a comment" do
+      let(:file) { test_file "comment.zip" }
+      specify { parser.end_of_cd_record_position.should eq 0xbf }
+    end
+
+    context "when parsing a file that is not a zip" do
+      let(:file) { test_file "not_a.zip" }
+      specify { lambda {parser.end_of_cd_record_position}.should raise_error }
     end
   end
 end
