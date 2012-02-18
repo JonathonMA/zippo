038610d66573de69d4c3f62ed209c9aa6f1b4f61
Jonathon M. Abbott <jma@dandaraga.net>
2012-02-18 16:50:47 +1000

    Add IO based uncompression

--- lib/zippo/uncompressor/deflate.rb
+++ lib/zippo/uncompressor/deflate.rb
@@ -1,22 +1,27 @@
 require 'zippo/uncompressor'
 
 require 'zlib'
+require 'stringio'
 
 module Zippo
   class DeflateUncompressor < Uncompressor
     METHOD = 8
+
     def initialize(io, compressed_size, zlib = Zlib::Inflate.new(-Zlib::MAX_WBITS)) # no header
       super(io, compressed_size)
       @zlib = zlib
       @block_size = 1 << 13
     end
+
     def uncompress
-      "".tap do |buf|
-        n, rest = @compressed_size.divmod @block_size
-        n.times { buf << @zlib.inflate(read @block_size) }
-        buf << @zlib.inflate(read rest)
-        buf << @zlib.finish
-      end
+      StringIO.new.tap{|x| uncompress_to(x) }.string
+    end
+
+    def uncompress_to io
+      n, rest = @compressed_size.divmod @block_size
+      n.times { io << @zlib.inflate(read @block_size) }
+      io << @zlib.inflate(read rest)
+      io << @zlib.finish
     end
   end
 end
--- spec/deflate_uncompressor_spec.rb
+++ spec/deflate_uncompressor_spec.rb
@@ -8,10 +8,16 @@ module Zippo
     let (:zstring) { Zlib::Deflate.new(Zlib::BEST_COMPRESSION, -Zlib::MAX_WBITS).deflate("a" * 20, Zlib::FINISH) }
     let (:size) { zstring.size }
     let (:string) { "aaa" + zstring + "bbb" }
+    let (:out) { StringIO.new }
     let (:io) { StringIO.new string }
     it "should deflate only the compressed string" do
       io.seek 3
       DeflateUncompressor.new(io, size).uncompress.should eq "a" * 20
+    end
+    it "should deflate to an IO" do
+      io.seek 3
+      DeflateUncompressor.new(io, size).uncompress_to(out)
+      out.string.should eq "a" * 20
     end
   end
 end
